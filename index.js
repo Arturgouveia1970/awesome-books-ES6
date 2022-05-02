5306a975e9f836f5c2d3a51d3cadc3feef212b5b
Arturgouveia1970 <artgouveiag@gmail.com>
2022-05-02 16:41:40 +0100

    added two modules

---
+++ index.js
@@ -0,0 +1,85 @@
+import {hide} from './modules/hideHtml.js';
+
+import {deleteBook} from './modules/deleteBooks.js';
+
+//import {addNewBook} from './modules/addingBook.js';
+
+
+class Book {
+    constructor() {
+      this.bookStore = JSON.parse(localStorage.getItem('singleBook')) || [];
+    }
+  
+    getBookList() {
+      const bookList = document.getElementById('book-list');
+      bookList.innerHTML = '';
+      bookList.innerHTML += this.bookStore.map(
+        (el, index) => ` <div class="${index % 2 === 0 ? 'dark' : 'mediumdark'}">
+          <h4>${el.title}</h4>
+          <p>${el.author}</p>
+          <button type="button" id=${el.id} class='remove-button'>Remove</button>
+          </div>`,
+      );
+      const Allbtn = document.querySelectorAll('.remove-button');
+      Allbtn.forEach((btn) => {
+        btn.addEventListener('click', () => {
+          this.deleteBook(btn.id);
+        });
+      });
+    }
+  
+    addNewBook(title, author) {
+      const id = this.bookStore.length + 1;
+      const addbook = {
+        title,
+        author,
+        id,
+      };
+      this.bookStore.push(addbook); localStorage.setItem('singleBook', JSON.stringify(this.bookStore));
+      this.getBookList();
+    }
+  
+  }
+  
+  const booksLibrary = new Book();
+  booksLibrary.getBookList();
+  const formData = document.getElementById('new-book');
+  formData.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const getTitle = document.getElementById('title').value;
+    const getAuthor = document.getElementById('author').value;
+    booksLibrary.addNewBook(getTitle, getAuthor);
+  });
+  
+  const listLink = document.getElementById('list-link');
+  const addLink = document.getElementById('add-link');
+  const contactLink = document.getElementById('contact-link');
+  const addDisplay = document.querySelector('.ADD');
+  const listDisplay = document.querySelector('.LIST');
+  const contactDisplay = document.querySelector('.contact');
+  
+  listLink.addEventListener('click', () => {
+    addDisplay.style.display = 'none';
+    contactDisplay.style.display = 'none';
+    listDisplay.style.display = 'flex';
+  });
+  
+  addLink.addEventListener('click', () => {
+    listDisplay.style.display = 'none';
+    contactDisplay.style.display = 'none';
+    addDisplay.style.display = 'flex';
+  });
+  
+  contactLink.addEventListener('click', () => {
+    listDisplay.style.display = 'none';
+    addDisplay.style.display = 'none';
+    contactDisplay.style.display = 'flex';
+  });
+
+window.onload = hide();
+
+/*window.onload = function () {
+    document.querySelector('.ADD').style.display = 'none';
+    document.querySelector('.contact').style.display = 'none';
+  };*/
+  
---
+++ modules/hideHtml.js
@@ -0,0 +1,4 @@
+export function hide () {
+    document.querySelector('.ADD').style.display = 'none';
+    document.querySelector('.contact').style.display = 'none';  
+}
