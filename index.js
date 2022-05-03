8db2de7e816c210c8b6d1a6b453e90dd0f9b6933
Arturgouveia1970 <artgouveiag@gmail.com>
2022-05-03 09:31:23 +0100

    luxon problems

--- index.js
+++ index.js
@@ -1,34 +1,43 @@
 import {hide} from './modules/hideHtml.js';
 
-import {deleteBook} from './modules/deleteBooks.js';
+//import {deleteBook} from './modules/deleteBooks.js';
 
-import {dateTime} from './modules/luxon.js';
+//import {dateTime} from './modules/luxon.js';
 
-window.onload = hide();
+import { DateTime } from "./modules/luxon";
 
+const Date =() => {
+  const getDate = document.querySelector('.date');
+  const time = () => {
+    const currentDateTime = DateTime.now().toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS);
+    getDate.innerHTML = currentDateTime;
+  };
+  setInterval(time, 1000);
+};
+Date();
 
 class Book {
-    constructor() {
-      this.bookStore = JSON.parse(localStorage.getItem('singleBook')) || [];
-    }
+  constructor() {
+    this.bookStore = JSON.parse(localStorage.getItem('singleBook')) || [];
+  }
   
-    getBookList() {
-      const bookList = document.getElementById('book-list');
-      bookList.innerHTML = '';
-      bookList.innerHTML += this.bookStore.map(
-        (el, index) => ` <div class="${index % 2 === 0 ? 'dark' : 'mediumdark'}">
-          <h4>${el.title}</h4>
-          <p>${el.author}</p>
-          <button type="button" id=${el.id} class='remove-button'>Remove</button>
-          </div>`,
-      );
-      const Allbtn = document.querySelectorAll('.remove-button');
-      Allbtn.forEach((btn) => {
-        btn.addEventListener('click', () => {
-          deleteBook(btn.id);
-        });
+  getBookList() {
+    const bookList = document.getElementById('book-list');
+    bookList.innerHTML = '';
+    bookList.innerHTML += this.bookStore.map(
+      (el, index) => ` <div class="${index % 2 === 0 ? 'dark' : 'mediumdark'}">
+        <h4>${el.title}</h4>
+        <p>${el.author}</p>
+        <button type="button" id=${el.id} class='remove-button'>Remove</button>
+        </div>`,
+    );
+    const Allbtn = document.querySelectorAll('.remove-button');
+    Allbtn.forEach((btn) => {
+      btn.addEventListener('click', () => {
+        this.deleteBook(btn.id);
       });
-    }
+    });
+  }
   
     addNewBook(title, author) {
       const id = this.bookStore.length + 1;
@@ -52,50 +61,47 @@ class Book {
       this.getBookList();
     }*/
 
-    
-    
+}
   
-  }
+const booksLibrary = new Book();
+booksLibrary.getBookList();
+const formData = document.getElementById('new-book');
+formData.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const getTitle = document.getElementById('title').value;
+  const getAuthor = document.getElementById('author').value;
+  booksLibrary.addNewBook(getTitle, getAuthor);
+});
   
-  const booksLibrary = new Book();
-  booksLibrary.getBookList();
-  const formData = document.getElementById('new-book');
-  formData.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const getTitle = document.getElementById('title').value;
-    const getAuthor = document.getElementById('author').value;
-    booksLibrary.addNewBook(getTitle, getAuthor);
-  });
+const listLink = document.getElementById('list-link');
+const addLink = document.getElementById('add-link');
+const contactLink = document.getElementById('contact-link');
+const addDisplay = document.querySelector('.ADD');
+const listDisplay = document.querySelector('.LIST');
+const contactDisplay = document.querySelector('.contact');
   
-  const listLink = document.getElementById('list-link');
-  const addLink = document.getElementById('add-link');
-  const contactLink = document.getElementById('contact-link');
-  const addDisplay = document.querySelector('.ADD');
-  const listDisplay = document.querySelector('.LIST');
-  const contactDisplay = document.querySelector('.contact');
+listLink.addEventListener('click', () => {
+  addDisplay.style.display = 'none';
+  contactDisplay.style.display = 'none';
+  listDisplay.style.display = 'flex';
+});
   
-  listLink.addEventListener('click', () => {
-    addDisplay.style.display = 'none';
-    contactDisplay.style.display = 'none';
-    listDisplay.style.display = 'flex';
-  });
+addLink.addEventListener('click', () => {
+  listDisplay.style.display = 'none';
+  contactDisplay.style.display = 'none';
+  addDisplay.style.display = 'flex';
+});
   
-  addLink.addEventListener('click', () => {
-    listDisplay.style.display = 'none';
-    contactDisplay.style.display = 'none';
-    addDisplay.style.display = 'flex';
-  });
-  
-  contactLink.addEventListener('click', () => {
-    listDisplay.style.display = 'none';
-    addDisplay.style.display = 'none';
-    contactDisplay.style.display = 'flex';
-  });
+contactLink.addEventListener('click', () => {
+  listDisplay.style.display = 'none';
+  addDisplay.style.display = 'none';
+  contactDisplay.style.display = 'flex';
+});
 
-//window.onload = hide();
+window.onload = hide();
 
-window.onload = function () {
+/*window.onload = function () {
     document.querySelector('.ADD').style.display = 'none';
     document.querySelector('.contact').style.display = 'none';
-  };
+  };*/
   
